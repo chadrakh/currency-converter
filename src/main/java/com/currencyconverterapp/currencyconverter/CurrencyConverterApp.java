b6a7a66be7f27bd6c6d2b951425c868eba3f5838
chadrakh <chadrak@outlook.com>
2023-05-25 10:10:29 +0100

    new entry point defined

--- src/main/java/com/currencyconverterapp/currencyconverter/CurrencyConverterApp.java
+++ src/main/java/com/currencyconverterapp/currencyconverter/CurrencyConverterApp.java
@@ -1,4 +1,7 @@
 package com.currencyconverterapp.currencyconverter;
 
 public class CurrencyConverterApp {
+    public static void main(String[] args) {
+        // entry point
+    }
 }
--- src/main/java/com/currencyconverterapp/currencyconverter/Main.java
+++
@@ -1,7 +0,0 @@
-package com.currencyconverterapp.currencyconverter;
-
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Currency Converter App");
-    }
-}
